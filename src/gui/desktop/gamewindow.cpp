6a72da6f337ebc67a2d934aa6018bfcafef774d8
Georgiy-JO <wkone.mail@gmail.com>
2025-04-03 09:41:03 +0700

    work in progress

---
+++ src/gui/desktop/gamewindow.cpp
@@ -0,0 +1,21 @@
+#include "gamewindow.h"
+#include "./ui_gamewindow.h"
+
+GameWindow::GameWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::GameWindow)
+{
+    ui->setupUi(this);
+}
+
+GameWindow::~GameWindow()
+{
+    delete ui;
+}
+
+void GameWindow::on_pushButton_clicked()
+{
+    ui->label_1->setText("Fuck you too!");
+}
+
+
